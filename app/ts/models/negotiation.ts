7f25aef94b3e064b01b6b47e3cd089fba0569028
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-03-31 13:44:49 -0300

    chore: readonly properties

--- app/ts/models/negotiation.ts
+++ app/ts/models/negotiation.ts
@@ -1,21 +1,8 @@
 export class Negotiation {
 
-    constructor (private _date: Date, private _amount: number, private _value: number) {}
-
-       // methods that give access  to these properties
-    get date() {
-        return this._date;
-    }
-
-    get amount() {
-        return this._amount;
-    }
-
-    get value() {
-        return this._value;
-    }
+    constructor (readonly date: Date, readonly amount: number, readonly value: number) {}
 
     get volume() {
-        return this._amount * this._value;
+        return this.amount * this.value;
     }
 }
